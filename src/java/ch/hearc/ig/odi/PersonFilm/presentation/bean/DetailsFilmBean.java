495c00d3812a7580ea860b63c609093be4b5d9a0
yasmine mabrouk <yasmine.mabrouk@he-arc.ch>
2015-12-13 17:44:17 +0100

    detail film xhtml et bean

---
+++ src/java/ch/hearc/ig/odi/PersonFilm/presentation/bean/DetailsFilmBean.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.PersonFilm.presentation.bean;
+
+import ch.hearc.ig.odi.PersonFilm.presentation.business.Movie;
+import ch.hearc.ig.odi.PersonFilm.presentation.business.Person;
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author yasmine.mabrouk
+ */
+@RequestScoped
+@Named(value = "detailFilmBean")
+public class DetailsFilmBean implements Serializable {
+Movie film ;
+    
+    public DetailsFilmBean() {
+    }
+
+    public Movie getFilm() {
+        return film;
+    }
+
+    public void setFilm(Movie film) {
+        this.film = film;
+    }
+    public String showFilm(Movie p ){
+           if(p != null){
+            film = p;
+        }else{
+            film = null;
+           }
+   return "ShowMovie";
+   }
+    
+
+}
