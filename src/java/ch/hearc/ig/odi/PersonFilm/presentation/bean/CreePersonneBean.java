ccd8196aeb9c0fa2ed33cc88ae1cd3cf5021765c
yasmine mabrouk <yasmine.mabrouk@he-arc.ch>
2015-12-17 08:05:46 +0100

    initiale commit branche

---
+++ src/java/ch/hearc/ig/odi/PersonFilm/presentation/bean/CreePersonneBean.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.PersonFilm.presentation.bean;
+
+import ch.hearc.ig.odi.PersonFilm.presentation.business.Person;
+import ch.hearc.ig.odi.PersonFilm.presentation.business.Services;
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author yasmine.mabrouk
+ */
+
+@SessionScoped
+@Named(value="creePersonneBean")
+public class CreePersonneBean implements Serializable {
+   @Inject private Services services;
+    Person personne ;
+
+    private CreePersonneBean(Person personne) {
+        this.personne = personne;
+    }
+
+    public Person getPersonne() {
+        return personne;
+    }
+
+    public void setPersonne(Person personne) {
+        this.personne = personne;
+    }
+    
+    public String addPerson (){
+        
+        if (personne != null){
+        services.getPeopleList().add(personne);
+         personne = null ;
+         return "addOk";
+        }else {
+          return "addError";
+        }
+    }
+    
+    
+    
+}
